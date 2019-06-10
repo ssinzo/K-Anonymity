417c8c54d698b6ee388c4ed658248c03bfc0a0d2
ssinzo <neoshin139@gmail.com>
2019-06-10 17:29:25 +0900

    time format fix

--- data_oneshot.py
+++ data_oneshot.py
@@ -3,22 +3,22 @@ from datetime import datetime
 
 time_stamp = list()
 
-time_stamp.append(['distance', datetime.now().strftime('%Y%m%d-%H  %M%S')])
+time_stamp.append(['distance', datetime.now().strftime('%Y/%m/%d - %H:%M:%S')])
 subprocess.run(['python ./data_distance.py'], shell=True)
 
-time_stamp.append(['convert', datetime.now().strftime('%Y%m%d-%H  %M%S')])
+time_stamp.append(['convert', datetime.now().strftime('%Y/%m/%d - %H:%M:%S')])
 subprocess.run(['python ./data_convert.py'], shell=True)
 
-time_stamp.append(['partition', datetime.now().strftime('%Y%m%d-%H  %M%S')])
+time_stamp.append(['partition', datetime.now().strftime('%Y/%m/%d - %H:%M:%S')])
 subprocess.run(['python ./data_partition.py'], shell=True)
 
-time_stamp.append(['build', datetime.now().strftime('%Y%m%d-%H  %M%S')])
+time_stamp.append(['build', datetime.now().strftime('%Y/%m/%d - %H:%M:%S')])
 subprocess.run(['python ./data_build.py'], shell=True)
 
-time_stamp.append(['recover', datetime.now().strftime('%Y%m%d-%H  %M%S')])
+time_stamp.append(['recover', datetime.now().strftime('%Y/%m/%d - %H:%M:%S')])
 subprocess.run(['python ./data_recover.py'], shell=True)
 
-time_stamp.append(['recover_count', datetime.now().strftime('%Y%m%d-%H  %M%S')])
+time_stamp.append(['recover_count', datetime.now().strftime('%%Y/%m/%d - %H:%M:%S')])
 subprocess.run(['python ./data_recover_count.py'], shell=True)
 
 
